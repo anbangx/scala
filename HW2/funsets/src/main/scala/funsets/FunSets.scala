af8491ea1fa767877b250f5bb6ea80e1cf424fba
Anbang Xu <axu@maprtech.com>
2014-12-10 14:44:17 -0800

    Add map function

--- HW2/funsets/src/main/scala/funsets/FunSets.scala
+++ HW2/funsets/src/main/scala/funsets/FunSets.scala
@@ -63,23 +63,33 @@ object FunSets {
    */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
-      if (???) ???
-      else if (???) ???
-      else iter(???)
+      if (a > bound) true
+      else if (!contains(s, a) || p(a)) iter(a + 1)
+      else false
     }
-    iter(???)
+    iter(-bound)
   }
 
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+//    def iter(a: Int): Boolean = {
+//      if (a > bound) false
+//      else if (contains(s, a) && p(a)) true
+//      else iter(a + 1)
+//    }
+//    iter(-bound)
+    !forall(s, (x : Int) => (!p(x)))
+  }
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = ???
+  def map(s: Set, f: Int => Int): Set = {
+    (x : Int) => exists(s, (elem : Int) => (f(elem) == x))
+  }
 
   /**
    * Displays the contents of a set
