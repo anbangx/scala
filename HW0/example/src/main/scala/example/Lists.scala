2432019b7e8e96617a0a10c263f41ccb721bc6aa
Anbang Xu <axu@maprtech.com>
2014-12-10 10:29:57 -0800

    Add recursive max and sum in HW0

--- HW0/example/src/main/scala/example/Lists.scala
+++ HW0/example/src/main/scala/example/Lists.scala
@@ -23,7 +23,7 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = if(xs.isEmpty) 0 else xs.head + sum(xs.tail)
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +38,14 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+	def maxHelper(xs : List[Int]) : Int = {
+	  if(xs.isEmpty) 
+	    Int.MinValue 
+	  else 
+	    Math.max(xs.head, maxHelper(xs.tail))}
+	
+    if(xs.isEmpty) throw new NoSuchElementException("Empty List")
+    else maxHelper(xs);
+  }
 }
